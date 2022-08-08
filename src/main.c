e099dc1d4065937cae911cf506b6a1db4fffa632
jumpycalm <jumpycalm@gmail.com>
2022-08-07 21:27:55 -0600

    v0.8.0

--- src/main.c
+++ src/main.c
@@ -68,7 +68,7 @@
 
 #define WHITE_SPACE "                                                                            "
 
-#define PN532_CLONER_VER "0.7.0"
+#define PN532_CLONER_VER "0.8.0"
 
 mftag t;
 mfreader r;
